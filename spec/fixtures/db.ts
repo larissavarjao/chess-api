797c1884cedf0c149a0805752fe33cb1a10cd2ad
Larissa <larissa.varjao@cubos.io>
2019-07-15 21:34:41 -0300

    test: add new tables to setup

--- spec/fixtures/db.ts
+++ spec/fixtures/db.ts
@@ -4,6 +4,16 @@ const deleteAllUsers = async () => {
   await sql`DELETE FROM users`;
 };
 
+const deleteAllMoves = async () => {
+  await sql`DELETE FROM moves`;
+};
+
+const deleteAllUsersMoves = async () => {
+  await sql`DELETE FROM users_moves`;
+};
+
 export const setupDB = async () => {
+  await deleteAllUsersMoves();
+  await deleteAllMoves();
   await deleteAllUsers();
 };
