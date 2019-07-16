88f9625a3ceb2abd7476c35e5c922fb38a4a8f1b
Larissa <larissa.varjao@cubos.io>
2019-07-15 21:35:14 -0300

    feature: add function to validate moves

---
+++ src/shared/validate.ts
@@ -0,0 +1,16 @@
+import 'jest-extended';
+
+export const validateMove = (move: string): boolean => {
+  const firstLetter = move[0];
+  const secondLetter = move[1];
+  const lowerThenLetterA = firstLetter.charCodeAt(0) < 65;
+  const biggerThenLetterH = firstLetter.charCodeAt(0) > 72;
+  const lowerThenNumber1 = parseInt(secondLetter) < 1;
+  const biggerThenNumber8 = parseInt(secondLetter) > 8;
+
+  if (move.length !== 2 || lowerThenLetterA || biggerThenLetterH || lowerThenNumber1 || biggerThenNumber8) {
+    return false;
+  }
+
+  return true;
+};
