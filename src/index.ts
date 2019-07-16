9daa4f4b386301591214adaafad2edc19938bbbd
Larissa <larissa.varjao@cubos.io>
2019-07-16 11:35:37 -0300

    feature: add body-parser again to test on heroku

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { router as userRouter } from './user/route';
 import { router as moveRouter } from './move/route';
 import './postgres';
 
 export const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use(moveRouter);
 
