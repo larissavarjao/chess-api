e292b9f4de3f5695246377548cdbbf1a79111588
Larissa <larissa.varjao@cubos.io>
2019-07-16 11:14:23 -0300

    refactor: only index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import { router as userRouter } from './user/route';
 import { router as moveRouter } from './move/route';
 import './postgres';
 
 export const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use(moveRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Listening on port ' + port);
+});
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './index';
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
-});
