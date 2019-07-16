5a423cc28d07e69d27c05ce1cf652270adf6b457
Larissa <larissa.varjao@cubos.io>
2019-07-15 21:34:07 -0300

    feature: add move route

---
+++ src/move/route.ts
@@ -0,0 +1,42 @@
+import * as express from 'express';
+import * as Move from './model';
+import { auth, RequestWithUser } from '../auth/auth';
+
+export const router = express.Router();
+
+router.get('/moves', auth, async (req: RequestWithUser, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(401).send();
+    }
+    const movesFromUser = await Move.getAllMovesFromUser(user!.id);
+    res.send({ moves: movesFromUser });
+  } catch (e) {
+    console.log('Error', e);
+    return res.status(400).send();
+  }
+});
+
+router.post('/moves', auth, async (req: RequestWithUser, res) => {
+  try {
+    const user = req.user;
+    const from = req.body.moveFrom;
+    const to = req.body.moveTo;
+    if (!user) {
+      return res.status(401).send();
+    }
+    if (!from) {
+      return res.status(401).send();
+    }
+    if (!to) {
+      return res.status(401).send();
+    }
+
+    const move = await Move.insert(user.id, from, to);
+    res.send({ move: Move.format(move) });
+  } catch (e) {
+    console.log('Error', e);
+    return res.status(400).send();
+  }
+});
