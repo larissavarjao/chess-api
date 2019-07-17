2a6f503b7d07d0ee79c82cadd981e05156211610
Larissa <larissa.varjao@cubos.io>
2019-07-17 15:54:47 -0300

    fix: moves route to verify if have moveFrom

--- src/move/route.ts
+++ src/move/route.ts
@@ -14,43 +14,13 @@ router.get('/moves', auth, async (req: RequestWithUser, res) => {
       return res.status(401).send();
     }
 
-    if (moveFrom) {
-      if (!validateMove(moveFrom)) {
-        return res.status(401).send();
-      }
-
-      const moves = await Move.getPossibilities(moveFrom);
-
-      return res.send({ moves });
-    }
-
-    console.log('passei aqui');
-
-    const movesFromUser = await Move.getAllMovesFromUser(user!.id);
-    res.send({ moves: movesFromUser });
-  } catch (e) {
-    console.log('Error', e);
-    return res.status(400).send();
-  }
-});
-
-router.post('/moves', auth, async (req: RequestWithUser, res) => {
-  try {
-    const user = req.user;
-    const from = req.body.moveFrom;
-    const to = req.body.moveTo;
-    if (!user) {
-      return res.status(401).send();
-    }
-    if (!from || !validateMove(from)) {
-      return res.status(401).send();
-    }
-    if (!to || !validateMove(to)) {
+    if (!moveFrom && !validateMove(moveFrom)) {
       return res.status(401).send();
     }
 
-    const move = await Move.insert(user.id, from, to);
-    res.send({ move: Move.format(move) });
+    const moves = await Move.getPossibilities(moveFrom);
+
+    return res.send({ moves });
   } catch (e) {
     console.log('Error', e);
     return res.status(400).send();
